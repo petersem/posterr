490f998d7e40d4d4d0c6105b7623aa7afd877b99
Matt P <mpetersen@pobox.com>
2021-04-04 16:37:44 +1000

    Finished basic UI layout (not wired up) and starting on settings class

---
+++ classes/core/settings.js
@@ -0,0 +1,19 @@
+class Settings {
+  constructor() {
+    return { error: "Not yet implemented" };
+  }
+
+  async GetSettings() {
+    return { error: "Not yet implemented" };
+  }
+
+  async SaveSettings() {
+    return { error: "Not yet implemented" };
+  }
+
+  async ValidateSettings() {
+    return { error: "Not yet implemented" };
+  }
+}
+
+module.exports = Settings;
